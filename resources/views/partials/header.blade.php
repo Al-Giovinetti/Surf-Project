c7b4eb6e5d51b870d40d3c08bdf460559fa6d667
Al-Giovinetti <giovinettialessio@gmail.com>
2024-02-01 19:05:24 +0100

    first version of the header created

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,22 @@
-<header>
-    <h1>Titolo X</h1>
-    <nav>
-        <ul>
-            <li>A</li>
-            <li>B</li>
-            <li>C</li>
-        </ul>
-    </nav>
+@php
+ $headerPages = ['boards','accessories','blog','technology','team','dealers'];
+ $socials= ['fa-brands fa-facebook-f','fa-brands fa-instagram','fa-solid fa-envelope']
+@endphp
+
+<header class="d-flex">
+    <h2>Surfers Co.</h2>
+    <ul class="d-flex">
+        @foreach ($headerPages as $pages)
+        <li>
+            <a href="">{{ strtoupper($pages) }}</a>
+        </li>
+        @endforeach
+    </ul>
+    <div class="socials d-flex">
+        @foreach ($socials as $social)
+            <div class="back-circle d-flex">
+                <i class="{{ $social }}"></i>
+            </div>
+        @endforeach
+    </div>
 </header>
