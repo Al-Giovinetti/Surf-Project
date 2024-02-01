1e79067e8155adaea810cab189b65c5510eaefad
Al-Giovinetti <giovinettialessio@gmail.com>
2024-02-01 21:35:14 +0100

    fix header in background

--- resources/views/boards.blade.php
+++ resources/views/boards.blade.php
@@ -6,7 +6,7 @@
 <div class="hero-zone">
     <div class="hero-text">
         <h1>Ride every wave as if it's your last</h1>
-        <p>We love the motion of the ocean</p>
+        <p class="c-white">We love the motion of the ocean</p>
     </div>
     <div class="bottom-shadow"></div>
 </div>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,20 +3,22 @@
  $socials= ['fa-brands fa-facebook-f','fa-brands fa-instagram','fa-solid fa-envelope']
 @endphp
 
-<header class="d-flex">
-    <h2>Surfers Co.</h2>
-    <ul class="d-flex">
-        @foreach ($headerPages as $pages)
-        <li>
-            <a href="">{{ strtoupper($pages) }}</a>
-        </li>
-        @endforeach
-    </ul>
-    <div class="socials d-flex">
-        @foreach ($socials as $social)
-            <div class="back-circle d-flex">
-                <i class="{{ $social }}"></i>
-            </div>
-        @endforeach
+<header>
+    <div class="header-container d-flex">
+        <h2>Surfers Co.</h2>
+        <ul class="d-flex">
+            @foreach ($headerPages as $pages)
+            <li>
+                <a href="">{{ strtoupper($pages) }}</a>
+            </li>
+            @endforeach
+        </ul>
+        <div class="socials d-flex">
+            @foreach ($socials as $social)
+                <div class="back-circle d-flex">
+                    <i class="{{ $social }} c-white"></i>
+                </div>
+            @endforeach
+        </div>
     </div>
 </header>
