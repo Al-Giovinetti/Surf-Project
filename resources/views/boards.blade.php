bbbd9688890b3227b439c5afaa5c80433fee15c2
Al-Giovinetti <giovinettialessio@gmail.com>
2024-02-02 08:42:52 +0100

    created a first vertion of article-board

--- resources/views/boards.blade.php
+++ resources/views/boards.blade.php
@@ -3,12 +3,45 @@
 @section("title","MAUI")
 
 @section("main")
-<div class="hero-zone">
-    <div class="hero-text">
-        <h1>Ride every wave as if it's your last</h1>
-        <p class="c-white">We love the motion of the ocean</p>
+<section>
+    <div class="hero-zone">
+        <div class="hero-text">
+            <h1>Ride every wave as if it's your last</h1>
+            <p class="c-white">We love the motion of the ocean</p>
+        </div>
+        <div class="bottom-shadow"></div>
     </div>
-    <div class="bottom-shadow"></div>
-</div>
+    <div class="buy-board">
+        <article class="board-card d-flex">
+            <div class="left-card">
+                <img src="" alt="aaaaaaaaaaaaaa">
+            </div>
+            <div class="center-card d-flex">
+                <img src="" alt="bbbbbb">
+                <img src="" alt="ccccccc">
+                <img src="" alt="ddddddd">
+                <img src="" alt="eeeeeee">
+            </div>
+            <div class="right-card">
+                <h3>Titolo</h3>
+                <div class="star-zone"></div>
+                <ul class="d-flex">
+                    <li>Description</li>
+                    <li>Features</li>
+                    <li>Dimensions</li>
+                </ul>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem mollitia officiis ut tempore sit vero aliquam non voluptatibus nihil iste, beatae possimus quod! Consequatur, obcaecati sunt. Beatae consectetur sit rem.
+                </p>
+            </div>
+        </article>
+        <p>ABOUT US</p>
+    </div>
+</section>
+
+<script>
+    
+</script>
+
 
 @endsection
